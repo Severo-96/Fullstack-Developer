368afd2d5a76e0eacc97d32788b0a78ff18a81f4
Davi Severino <daviseverino96@gmail.com>
2025-11-11 00:03:20 -0300

    backend update

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -73,6 +73,11 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   config.action_view.annotate_rendered_view_with_filenames = true
 
+  config.action_cable.allowed_request_origins = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173'
+  ]
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
